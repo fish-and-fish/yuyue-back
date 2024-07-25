56c65bf50c2bb3455c98ef2fa0d6fe0c487f9d3e
yehuangyuan <ye.huangyuan@darcytech.com>
2024-07-25 17:29:59 +0800

    1

---
+++ src/test/java/com/amrni/sport/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrni.sport.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
